2425766a8373714341a3209da60fae02e9c539f6
SmutnyJan <57211415+SmutnyJan@users.noreply.github.com>
2021-12-03 19:45:15 +0100

    first prototype finished

--- main.ts
+++ main.ts
@@ -1,1 +1,20 @@
-
+input.onButtonPressed(Button.A, function () {
+    if (!(isGuarding)) {
+        basic.pause(5000)
+        basic.showIcon(IconNames.Asleep)
+    } else {
+        basic.showIcon(IconNames.Happy)
+    }
+    isGuarding = !(isGuarding)
+})
+let isGuarding = false
+let toleration = 100
+basic.showIcon(IconNames.Happy)
+basic.forever(function () {
+    serial.writeLine("" + (input.acceleration(Dimension.Strength)))
+    if ((input.acceleration(Dimension.Strength) + toleration < 1050 || input.acceleration(Dimension.Strength) - toleration > 1050) && isGuarding) {
+        basic.showIcon(IconNames.Angry)
+        soundExpression.sad.playUntilDone()
+        basic.showIcon(IconNames.Asleep)
+    }
+})
