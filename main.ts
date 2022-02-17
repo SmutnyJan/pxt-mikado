776e67cf3218caaa3133680090cd254345ad600a
SmutnyJan <57211415+SmutnyJan@users.noreply.github.com>
2022-02-18 00:50:01 +0100

    extension added

--- main.ts
+++ main.ts
@@ -1,30 +1,16 @@
 input.onButtonPressed(Button.A, function () {
     if (isGuarding == false) {
-        music.setBuiltInSpeakerEnabled(true)
-        basic.pause(2500)
-        basic.showIcon(IconNames.Asleep)
+        Mikado.ZapnoutHlidani()
         isGuarding = true
     } else {
-        music.setBuiltInSpeakerEnabled(false)
+        Mikado.VypnoutHlidani()
         isGuarding = false
-        basic.showIcon(IconNames.Happy)
     }
 })
 let isGuarding = false
-let toleration = 100
-basic.showIcon(IconNames.Happy)
+Mikado.VychoziNastaveni(90)
 basic.forever(function () {
-    serial.writeLine("" + (input.acceleration(Dimension.Strength)))
-    if ((input.acceleration(Dimension.Strength) + toleration < 1050 || input.acceleration(Dimension.Strength) - toleration > 1050) && isGuarding) {
-        basic.showIcon(IconNames.Angry)
-        soundExpression.sad.playUntilDone()
-        // zde dochází k zajímavému problému: když zmáčknu tlačítko A, mezitím co
-        // hraje melodie, přepne se displej do Happy. Ovšak po skončení melodie se
-        // provede zobrazení Asleep. Proto
-        if (!(isGuarding)) {
-            basic.showIcon(IconNames.Happy)
-        } else {
-            basic.showIcon(IconNames.Asleep)
-        }
+    if (Mikado.StavHlidani() && isGuarding) {
+        Mikado.VzbuditHlidace()
     }
 })
--- mikado.ts
+++ mikado.ts
@@ -10,13 +10,66 @@
 //% weight=100 color=#3bccc0 icon="\uf11b"
 namespace Mikado {
 
+    let toleration = 100
+    let isGuarding = false
     /**
-    * Nastaví hodnoty pro různé hlasovací možnosti
+    * Nastaví toleranci
     */
-    //% block="Nastav hlasovací hodnoty A: %a B: %b C: %c D: %d"
+    //% block="Nastav toleranci %tolerance"
 
-    export function VychoziNastaveni(Aoption: string, Boption: string, Coption: string, Doption: string): void {
+    export function VychoziNastaveni(tolerance: number): void {
+        toleration = tolerance
+    }
 
+    /**
+    * Zapne hlídání
+    */
+    //% block="Zapni hlídání"
+
+    export function ZapnoutHlidani(): void {
+        music.setBuiltInSpeakerEnabled(true)
+        basic.pause(2500)
+        basic.showIcon(IconNames.Asleep)
+        isGuarding = true
+    }
+
+    /**
+    * Vypne hlídání
+    */
+    //% block="Vypni hlídání"
+
+    export function VypnoutHlidani(): void {
+        music.setBuiltInSpeakerEnabled(false)
+        basic.showIcon(IconNames.Happy)
+        isGuarding = false
+
+    }
+
+    /**
+    * Vrátí true/false podle toho, jestli došlo k porušení hlídání
+    */
+    //% block="Stav hlídání"
+
+    export function StavHlidani(): boolean {
+        if (input.acceleration(Dimension.Strength) + toleration < 1050 || input.acceleration(Dimension.Strength) - toleration > 1050) {
+            return true
+        }
+        return false
+    }
+
+    /**
+    * Vzbudí hlídače
+    */
+    //% block="Vzbuď hlídače"
+
+    export function VzbuditHlidace(): void {
+        basic.showIcon(IconNames.Angry)
+        soundExpression.sad.playUntilDone()
+        if (!(isGuarding)) {
+            basic.showIcon(IconNames.Happy)
+        } else {
+            basic.showIcon(IconNames.Asleep)
+        }
     }
 
 }
