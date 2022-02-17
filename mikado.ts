215031996929d7b2cbbbf7a69ee3b41cbd5ee85e
SmutnyJan <57211415+SmutnyJan@users.noreply.github.com>
2022-02-17 23:20:25 +0100

    Aktualizovat pxt.json, main.blocks, mikado.ts

---
+++ mikado.ts
@@ -0,0 +1,22 @@
+/**
+ * Použijte tento soubor k definování personalizovaných funkcí a bloků.
+ * Přečtěte si více na https://makecode.microbit.org/blocks/custom
+ */
+
+
+/**
+ * Custom blocks
+ */
+//% weight=100 color=#3bccc0 icon="\uf11b"
+namespace Mikado {
+
+    /**
+    * Nastaví hodnoty pro různé hlasovací možnosti
+    */
+    //% block="Nastav hlasovací hodnoty A: %a B: %b C: %c D: %d"
+
+    export function VychoziNastaveni(Aoption: string, Boption: string, Coption: string, Doption: string): void {
+
+    }
+
+}
